686796c72e1c458f9c491fa4313374cb60f8a794
Rita Kikian <kikianrita@gmail.com>
2025-02-18 19:26:48 +0200

    Improve structure with allocated variables

--- financial-calculator.py
+++ financial-calculator.py
@@ -7,12 +7,15 @@ electricity = float(input("enter percentage of electricity: "))
 
 #print the financial details
 print("your salary for the month of ", month_name, " is: ", salary)
-print("your savings for the month of ", month_name, " is: ", salary * savings / 100)
-print("your rent for the month of ", month_name, " is: ", salary * rent / 100)
-print("your electricity for the month of ", month_name, " is: ", salary * electricity / 100)
+allocated_savings = salary * savings / 100
+print("your savings for the month of ", month_name, " is: ", allocated_savings)
+allocated_rent = salary * rent / 100
+print("your rent for the month of ", month_name, " is: ", allocated_rent)
+allocated_electricity = salary * electricity / 100
+print("your electricity for the month of ", month_name, " is: ", allocated_electricity)
 
 #calculate the total expenses
-total_expenses = (salary * savings / 100) + (salary * rent / 100) + (salary * electricity / 100)
+total_expenses = (allocated_savings) + (allocated_rent) + (allocated_electricity)
 print ("your total expenses for the month of ", month_name, " is: ", total_expenses)
 
 #calculate the remainder after expenses
@@ -20,8 +23,8 @@ remainder = salary - total_expenses
 print("your remainder for the month of ", month_name, " is: ", remainder)
 
 #calculate yearly expenses on rent and electricity
-yearly_rent = 12 * (salary * rent / 100)
-yearly_electricity = 12 * (salary * electricity / 100)
+yearly_rent = 12 * (allocated_rent)
+yearly_electricity = 12 * (allocated_electricity)
 print("your total yearly expenses on rent and electricity: ", yearly_rent + yearly_electricity)
 
 #calculate salary ^2
@@ -30,7 +33,7 @@ print("your salary squared is: ", salary_squared)
 
 #calculate total savings
 additional_savings = float(input("enter additional savings: "))
-total_savings = (salary * savings / 100) + additional_savings 
+total_savings = (allocated_savings) + additional_savings 
 
 if total_savings > 0:  
     remaining_amount = (additional_savings / total_savings) * 100 
